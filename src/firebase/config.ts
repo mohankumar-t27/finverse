ddcd8337d063e60e159c385543672d508bca6b6f
Mohankumar Thangavel <mohangethack27@gmail.com>
2025-12-07 17:17:00 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,10 @@
 // This file is intentionally left blank. 
 // The Firebase config will be populated by the backend.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-2880153807-25572",
+  "appId": "1:655681884332:web:f13dc732e01d0fa2d7cf09",
+  "apiKey": "AIzaSyCF3sKsV3wdjNCrOaTTKOlz3I9DlK3GwXo",
+  "authDomain": "studio-2880153807-25572.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "655681884332"
+};
