ba9bb39dc9a0526fb245c3f2fd847ac7a236ce44
Mohankumar Thangavel <mohangethack27@gmail.com>
2025-12-07 15:54:44 +0000

    apart from header, change ruppee to Rs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number) {
-  return new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-  }).format(amount);
+  return `Rs ${new Intl.NumberFormat('en-IN').format(amount)}`;
 }
