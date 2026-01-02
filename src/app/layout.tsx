ce6dba28ee129e71a31684165684ea04efad9b3a
Mohankumar Thangavel <mohangethack27@gmail.com>
2026-01-02 15:55:16 +0000

    Change app name to Finverse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 import FirebaseClientProvider from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
-  title: 'ExpenseWise',
+  title: 'Finverse',
   description: 'Track and manage your monthly expenses.',
 };
 
